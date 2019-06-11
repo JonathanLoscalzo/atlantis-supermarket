8d902a4663e65882b2eecdaf1b92f94c121be064
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2019-06-10 21:35:56 -0300

    edit admin menu. show reference for batch table

--- client-app/supermarket-atlantis/src/menu.js
+++ client-app/supermarket-atlantis/src/menu.js
@@ -1,32 +1,33 @@
+import { isAdmin } from './common/auth'
 const menu_factory = (title, to, icon, children = []) => ({ title, to, icon, children })
 
-// TODO: config file with admin and client and user routes
-const menu = [
-    // parte admin
-    menu_factory("Productos", "/product", "icon-basket", [
+let menu = []
+
+if (isAdmin) {
+  menu =
+    [menu_factory("Productos", "/product", "icon-basket", [
       menu_factory("Productos", "/product", "icon-basket"),
       menu_factory("Categorias", "/category", "icon-basket"),
       menu_factory("Proveedores", "/provider", "icon-basket"),
     ]),
-    
+
     menu_factory("Inventario", "/batch", "icon-basket", [
       menu_factory("Lotes", "/batch", "icon-basket"),
-      menu_factory("Abastecer", "/batch/add", "icon-basket"),
     ]),
-  
+
     menu_factory("Clientes", "/clients", "icon-basket", [
       menu_factory("Ver", "/clients", "icon-basket"),
     ]),
-  
-  
+
     menu_factory("Ventas", "", "icon-basket", [
       menu_factory("Ver", "/sale", "icon-basket"),
-    ]),
-  
-    //parte cliente
+    ])]
+} else {
+  menu = [
     menu_factory("Catalogo", "", "icon-basket", [
       menu_factory("Ver", "/car", "icon-basket"),
     ]),
-  ];
+  ]
+}
 
 export default menu;
--- client-app/supermarket-atlantis/src/modules/products/admin/batches/list/presentational/ProductList.js
+++ client-app/supermarket-atlantis/src/modules/products/admin/batches/list/presentational/ProductList.js
@@ -3,65 +3,74 @@ import columns from './ColumnsConfig'
 import ReactTable from 'react-table'
 import _ from 'lodash'
 import NuevoButton from '../../../../../shared/NewElement'
-import { Input } from 'reactstrap'
+import { Input, Card, CardBody } from 'reactstrap'
 
 const ProductList = (props) => {
     return (
         <div className="col">
-            <div className="col">
-                <NuevoButton goToCreate={props.goToCreate} /> <br />
+            <div className="row">
+                <div className="col-3">
+                    <NuevoButton goToCreate={props.goToCreate} /> <br />
+                </div>
+                <div className="col-3">
+                        <h4>Filtrado</h4>
+                        <Input type="checkbox"
+                            checked={props.checked}
+                            onChange={props.handlePivot} />{' '}
+                        Pivotear Tipo
+                </div>
+                <div className="col-3">
+                    <h4>Referencia</h4>
+                    <p class="text-danger">Expirado</p>
+                    <p class="text-warning">Stock Bajo</p>
+                </div>
             </div>
-            <div className="col">
-                <Input type="checkbox"
-                    checked={props.checked}
-                    onChange={props.handlePivot} />{' '}
-                Pivotear
+            <div className="row-fluid">
+                <ReactTable
+                    {...props}
+                    manual
+                    data={props.data}
+                    pages={props.pages}
+                    loading={props.data_loading}
+                    onFetchData={props.fetchData}
+                    onPageSizeChange={props.onPageSizeChange}
+                    columns={columns.productColumns}
+                    pivotBy={props.pivotBy}
+                    defaultPageSize={props.defaultPageSize}
+                    className="-striped -highlight"
+                    // getTrProps={(state, rowInfo, column, instance) => {
+                    //     if (rowInfo && !rowInfo.aggregated) {
+                    //         let { original } = rowInfo;
+                    //         let lowStock = original.minStock < _.sum(original.batches.map(x => x.remainingUnits))
+                    //         return {
+                    //             style: {
+                    //                 background: lowStock ? '' : 'red'
+                    //             }
+                    //         }
+                    //     }
+
+                    //     return {
+                    //         style: {
+                    //             //background: true ? 'green' : 'red'
+                    //         }
+                    //     }
+
+                    // }}
+                    SubComponent={row => {
+                        return (
+                            <div style={{ padding: "20px" }}>
+                                <h3> Lotes </h3>
+                                <ReactTable
+                                    data={row.original.batches}
+                                    columns={columns.columnsBatch}
+                                    defaultPageSize={3}
+                                />
+                            </div>
+                        );
+                    }}
+                />
             </div>
-            <br />
-            <ReactTable
-                {...props}
-                manual
-                data={props.data}
-                pages={props.pages}
-                loading={props.data_loading}
-                onFetchData={props.fetchData}
-                onPageSizeChange={props.onPageSizeChange}
-                columns={columns.productColumns}
-                pivotBy={props.pivotBy}
-                defaultPageSize={props.defaultPageSize}
-                className="-striped -highlight"
-                // getTrProps={(state, rowInfo, column, instance) => {
-                //     if (rowInfo && !rowInfo.aggregated) {
-                //         let { original } = rowInfo;
-                //         let lowStock = original.minStock < _.sum(original.batches.map(x => x.remainingUnits))
-                //         return {
-                //             style: {
-                //                 background: lowStock ? '' : 'red'
-                //             }
-                //         }
-                //     }
-
-                //     return {
-                //         style: {
-                //             //background: true ? 'green' : 'red'
-                //         }
-                //     }
-
-                // }}
-                SubComponent={row => {
-                    return (
-                        <div style={{ padding: "20px" }}>
-                            <h3> Lotes </h3>
-                            <ReactTable
-                                data={row.original.batches}
-                                columns={columns.columnsBatch}
-                                defaultPageSize={3}
-                            />
-                        </div>
-                    );
-                }}
-            />
-        </div>
+        </div >
     )
 }
 
