8748368d7e5dc77dd950d8c804a09513dc404104
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2019-07-09 13:40:02 -0300

    presentación view de cliente

--- client-app/supermarket-atlantis/src/modules/clients/view/presentation/Presentation.js
+++ client-app/supermarket-atlantis/src/modules/clients/view/presentation/Presentation.js
@@ -5,21 +5,23 @@ const Presentation = (props) => {
     const { element } = props;
 
     return (
-        <Row>
-            <Col sm="12">
-                <Button onClick={props.goBack} >Volver</Button>
-            </Col>
-            <Col sm="4">
-                <Card body>
-                    <CardTitle>Cliente</CardTitle>
-                    <CardText>
-                        {element.email}
-                        {element.name}, {element.surname}
-                        {element.username}
-                    </CardText>
-                </Card>
-            </Col>
-        </Row>
+        <React.Fragment>
+            <Row>
+                <Col sm="12">
+                    <Button onClick={props.goBack} >Volver</Button>
+                </Col>
+                <Col sm="6" className={"p-3"}>
+                    <Card body>
+                        <CardTitle><h5>Cliente</h5></CardTitle>
+                        <CardText>
+                            <p>email: {element.email}</p>
+                            <p>fullname: {element.name}, {element.surname}</p>
+                            <p>username: {element.username}</p>
+                        </CardText>
+                    </Card>
+                </Col>
+            </Row>
+        </React.Fragment>
     )
 }
 
