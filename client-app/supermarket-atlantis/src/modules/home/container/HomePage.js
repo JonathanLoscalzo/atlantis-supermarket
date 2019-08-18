4c9c6f1ca80793bb1f5f4684c634bdd536e96759
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2019-08-18 18:50:32 -0300

    agregar gráficos de superset

--- client-app/supermarket-atlantis/src/modules/home/container/HomePage.js
+++ client-app/supermarket-atlantis/src/modules/home/container/HomePage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { loaded } from '../index'
 import Layout from '../../../components/Layout';
+import { isAdmin } from 'common/auth';
 
 class HomePage extends React.Component {
 
@@ -12,104 +13,64 @@ class HomePage extends React.Component {
 
     render() {
         const { loaded } = this.props;
+        if (isAdmin()) {
 
-        return (
-            // <div class="col-md-8">
-            //     <div class="card">
-            //         <div class="card-header">
-            //             <h5 class="title">Edit Profile</h5>
-            //         </div>
-            //         <div class="card-body">
-            //             <form>
-            //                 <div class="row">
-            //                     <div class="col-md-5 pr-1">
-            //                         <div class="form-group">
-            //                             <label>Company (disabled)</label>
-            //                             <input type="text" class="form-control" disabled="" placeholder="Company" value="Creative Code Inc." />
-            //                         </div>
-            //                     </div>
-            //                     <div class="col-md-3 px-1">
-            //                         <div class="form-group">
-            //                             <label>Username</label>
-            //                             <input type="text" class="form-control" placeholder="Username" value="michael23" />
-            //                         </div>
-            //                     </div>
-            //                     <div class="col-md-4 pl-1">
-            //                         <div class="form-group">
-            //                             <label for="exampleInputEmail1">Email address</label>
-            //                             <input type="email" class="form-control" placeholder="Email" />
-            //                         </div>
-            //                     </div>
-            //                 </div>
-
-            //                 <div class="row">
-            //                     <div class="col-md-6 pr-1">
-            //                         <div class="form-group">
-            //                             <label>First Name</label>
-            //                             <input type="text" class="form-control" placeholder="Company" value="Mike" />
-            //                         </div>
-            //                     </div>
-            //                     <div class="col-md-6 pl-1">
-            //                         <div class="form-group">
-            //                             <label>Last Name</label>
-            //                             <input type="text" class="form-control" placeholder="Last Name" value="Andrew" />
-            //                         </div>
-            //                     </div>
-            //                 </div>
-
-            //                 <div class="row">
-            //                     <div class="col-md-12">
-            //                         <div class="form-group">
-            //                             <label>Address</label>
-            //                             <input type="text" class="form-control" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" />
-            //                         </div>
-            //                     </div>
-            //                 </div>
-
-            //                 <div class="row">
-            //                     <div class="col-md-4 pr-1">
-            //                         <div class="form-group">
-            //                             <label>City</label>
-            //                             <input type="text" class="form-control" placeholder="City" value="Mike" />
-            //                         </div>
-            //                     </div>
-            //                     <div class="col-md-4 px-1">
-            //                         <div class="form-group">
-            //                             <label>Country</label>
-            //                             <input type="text" class="form-control" placeholder="Country" value="Andrew" />
-            //                         </div>
-            //                     </div>
-            //                     <div class="col-md-4 pl-1">
-            //                         <div class="form-group">
-            //                             <label>Postal Code</label>
-            //                             <input type="number" class="form-control" placeholder="ZIP Code" />
-            //                         </div>
-            //                     </div>
-            //                 </div>
-
-            //                 <div class="row">
-            //                     <div class="col-md-12">
-            //                         <div class="form-group">
-            //                             <label>About Me</label>
-            //                             <textarea rows="4" cols="80" class="form-control" placeholder="Here can be your description" value="Mike">Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</textarea>
-            //                         </div>
-            //                     </div>
-            //                 </div>
-            //             </form>
-            //         </div>
-            //     </div>
-            // </div>
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="title">Home</h5>
-                    </div>
-                    <div class="card-body">
-                        aloha
+            return (
+                <div className="row">
+                    <div className="col-md-10">
+                        <div className="card">
+                            <div className="card-header">
+                                <h5 className="title">Ventas</h5>
+                            </div>
+                            <div className="card-body">
+                                <iframe
+                                    width="600"
+                                    height="300"
+                                    seamless
+                                    frameBorder="0"
+                                    scrolling="no"
+                                    src="http://127.0.0.1:8088/superset/explore/?form_data=%7B%22datasource%22%3A%2212__table%22%2C%22viz_type%22%3A%22dist_bar%22%2C%22slice_id%22%3A76%2C%22url_params%22%3A%7B%7D%2C%22granularity_sqla%22%3Anull%2C%22time_grain_sqla%22%3A%22P1D%22%2C%22time_range%22%3A%22Last+week%22%2C%22metrics%22%3A%5B%22count%22%5D%2C%22adhoc_filters%22%3A%5B%5D%2C%22groupby%22%3A%5B%22name%22%5D%2C%22columns%22%3A%5B%5D%2C%22row_limit%22%3A10000%2C%22contribution%22%3Afalse%2C%22color_scheme%22%3A%22d3Category10%22%2C%22show_legend%22%3Atrue%2C%22show_bar_value%22%3Atrue%2C%22bar_stacked%22%3Afalse%2C%22order_bars%22%3Afalse%2C%22y_axis_format%22%3A%22.3s%22%2C%22y_axis_label%22%3A%22%22%2C%22show_controls%22%3Afalse%2C%22x_axis_label%22%3A%22prods%22%2C%22bottom_margin%22%3A50%2C%22x_ticks_layout%22%3A%2245%C2%B0%22%2C%22reduce_x_ticks%22%3Atrue%7D&standalone=true&height=200"
+                                >
+                                </iframe>
+                            </div>
                         </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="card">
+                            <div className="card-header">
+                                <h5 className="title">Ganancias último mes</h5>
+                            </div>
+                            <div className="card-body">
+                                <iframe
+                                    width="200"
+                                    height="100"
+                                    seamless
+                                    frameBorder="0"
+                                    scrolling="yes"
+                                    src="http://127.0.0.1:8088/superset/explore/?form_data=%7B%22datasource%22%3A%2212__table%22%2C%22viz_type%22%3A%22big_number_total%22%2C%22slice_id%22%3A77%2C%22url_params%22%3A%7B%7D%2C%22granularity_sqla%22%3A%22created_date%22%2C%22time_grain_sqla%22%3Anull%2C%22time_range%22%3A%22Last+month%22%2C%22metric%22%3A%7B%22expressionType%22%3A%22SIMPLE%22%2C%22column%22%3A%7B%22id%22%3A445%2C%22column_name%22%3A%22profit%22%2C%22verbose_name%22%3A%22%22%2C%22description%22%3A%22ganancias%22%2C%22expression%22%3A%22%22%2C%22filterable%22%3Atrue%2C%22groupby%22%3Afalse%2C%22is_dttm%22%3Afalse%2C%22type%22%3A%22DECIMAL%2819%2C2%29%22%2C%22database_expression%22%3A%22%22%2C%22python_date_format%22%3A%22%22%2C%22optionName%22%3A%22_col_profit%22%7D%2C%22aggregate%22%3A%22SUM%22%2C%22sqlExpression%22%3Anull%2C%22hasCustomLabel%22%3Afalse%2C%22fromFormData%22%3Atrue%2C%22label%22%3A%22SUM%28profit%29%22%2C%22optionName%22%3A%22metric_7n6be0x99ly_hskrjv9gn%22%7D%2C%22adhoc_filters%22%3A%5B%5D%2C%22y_axis_format%22%3A%22.3s%22%7D&standalone=true&height=100"
+                                >
+                                </iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div >
+            )
+        } else {
+            return (
+                <div>
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">
+                                <h5 className="title">Bienvenido! </h5>
+                            </div>
+                            <div className="card-body">
+                                Puede revisar sus ventas o comprar nuevos elementos
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
